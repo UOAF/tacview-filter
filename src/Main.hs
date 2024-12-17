618a436b206c4f699875da67f128fe2729b247a1
Matt Kline <matt@bitbashing.io>
2024-12-17 09:00:40 +0000

    Native ZIP handling
    I can into Conduit

--- src/Main.hs
+++ src/Main.hs
@@ -4,12 +4,22 @@
 
 module Main where
 
+import Codec.Archive.Zip
 import Control.Concurrent.Channel
 import Control.Concurrent.STM
-import Data.Time.Clock.POSIX
+import Control.Monad
+import Conduit (liftIO)
+import Data.Conduit ((.|))
+import Data.Conduit qualified as C
+import Data.Conduit.Combinators qualified as C
+import Data.IORef
+import Data.List
+import Data.Map.Strict qualified as M
 import Data.Ratio
 import Data.Text (Text)
+import Data.Text qualified as T
 import Data.Text.IO qualified as T
+import Data.Time.Clock.POSIX
 import Options.Applicative
 import System.IO
 import Text.Printf
@@ -17,13 +27,13 @@ import Text.Printf
 import Delta
 import Ignores
 
--- No command line args at the moment...
--- Add flags to --ignore additional text?
--- Or we can just change this short program...
-data Args = Args
+newtype Args = Args (Maybe FilePath)
 
 parseArgs :: Parser Args
-parseArgs = pure Args
+parseArgs = fmap Args $ optional . strArgument $ mconcat [
+    help "Zipped ACMI to filter. Otherwise reads from stdin and writes to stdout",
+    metavar "recording.zip.acmi"
+    ]
 
 -- --help text
 main :: IO ()
@@ -35,11 +45,11 @@ main = do
 
     let parser = customExecParser (prefs showHelpOnError) parseInfo
         parseInfo = info (parseArgs <**> helper) $
-            progDesc "Reads ACMI on stdin, filters crap, writes to stdout"
+            progDesc "Ensmallens ACMI files"
     parser >>= runFilter
 
 runFilter :: Args -> IO ()
-runFilter _ = do
+runFilter (Args maybeZip) = do
     start <- getPOSIXTime
 
     -- Hoare Was Right.
@@ -47,9 +57,11 @@ runFilter _ = do
     -- Major steps are run in their own task.
     -- They filters or modify lines, then pass the remainders to the next stage of the pipe.
     -- This parallelizes trivially; the runtime runs each task in a free thread.
-    let ignore sink = pipeline (reader 0) (\source -> filterLines Ignores.startState source sink)
+    let filterInput = maybe (reader 0) readZip maybeZip
+        filterOutput = maybe (write 0) writeZip maybeZip
+        ignore sink = pipeline filterInput (\source -> filterLines Ignores.startState source sink)
         thenDeltas sink = pipeline ignore (\source -> deltas Delta.startState source sink)
-        thenDropTimes = pipeline thenDeltas (write 0)
+        thenDropTimes = pipeline thenDeltas filterOutput
 
     -- Gather up all our stats, placed in newtypes for easier readability here.
     (((InputLines i, FilteredLines f), DecimatedLines d), OutputLines o) <- thenDropTimes
@@ -84,6 +96,20 @@ reader !l c = do
             T.getLine >>= evalWriteChannel c
             reader (l + 1) c
 
+zipExt :: String
+zipExt = ".zip.acmi"
+
+readZip :: FilePath -> Channel Text -> IO InputLines
+readZip z c = do
+    unless (zipExt `isSuffixOf` z) $ error "Expected .zip.acmi file"
+    withArchive z $ do
+        e <- getEntries
+        when (M.null e) $ error "empty ZIP archive!"
+        let sel = head $ M.keys e
+        l <- sourceEntry sel $
+            C.decodeUtf8 .| C.linesUnbounded .| C.iterM (liftIO . evalWriteChannel c) .| C.length
+        pure $ InputLines l
+
 newtype DroppedTimeLines = DroppedTimeLines Int
 newtype OutputLines = OutputLines Int
 
@@ -94,3 +120,22 @@ write !count source = atomically (readChannel source) >>= \case
     Just l -> do
         T.putStrLn l
         write (count + 1) source
+
+writeZip :: FilePath -> Channel Text -> IO OutputLines
+writeZip z c = do
+    let zn = (T.unpack . T.dropEnd (length zipExt) . T.pack $ z) <> "-filtered" <> zipExt
+    ol <- newIORef 0
+    createArchive zn $ do
+        sel <- mkEntrySelector "acmi.txt"
+        let src !i = liftIO (atomically (readChannel c)) >>= \case
+                Nothing -> do
+                    liftIO $ writeIORef ol i -- HACK HACK - make sinkEntry return a value?
+                    pure ()
+                Just l -> do
+                    C.yield l
+                    src (i + 1)
+            pipe = src 0 .| C.unlines .| C.encodeUtf8
+        sinkEntry Deflate pipe sel
+    OutputLines <$> readIORef ol
+
+
