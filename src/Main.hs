18ec02fa59d6b54a6282c1878c7ea92aa8a77da2
Matt Kline <matt@bitbashing.io>
2024-12-23 19:44:37 -0700

    NOLEAK, quit yer deferring

--- src/Main.hs
+++ src/Main.hs
@@ -6,7 +6,9 @@
 module Main where
 
 import Conduit
+import Codec.Archive.Zip.Internal qualified as ZI
 import Codec.Archive.Zip
+import Data.Map.Strict qualified as M
 import System.IO
 
 main :: IO ()
@@ -17,5 +19,17 @@ main = do
     hSetNewlineMode stderr noNewlineTranslation
 
     let src = stdinC .| decodeUtf8C .| linesUnboundedC
-    let dest = unlinesC .| encodeUtf8C .| sinkFile "out.txt"
-    runConduitRes $ src .| dest
+    withBinaryFile "out.zip" WriteMode $ \h -> do
+        sel <- mkEntrySelector "out.zip.acmi"
+        let eaCompression = M.singleton sel Deflate
+            eaEntryComment = M.empty
+            eaDeleteComment = M.empty
+            eaModTime = M.empty
+            eaExtraField = M.empty
+            eaDeleteField = M.empty
+            eaExtFileAttr = M.empty
+            ea = ZI.EditingActions{..}
+            pipe = src .| unlinesC .| encodeUtf8C
+        (es, des) <- ZI.sinkEntry h sel ZI.GenericOrigin pipe ea
+        let cdmap = M.singleton es des
+        ZI.writeCD h Nothing cdmap
