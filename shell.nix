5b2f8efb2cee435c13d80382e666b5d6cdee6cee
Matt Kline <matt@bitbashing.io>
2025-02-09 20:52:38 -0800

    Bump to Stack 23.7
    NB: exactDeps is load-bearing.

--- shell.nix
+++ shell.nix
@@ -1,10 +1,32 @@
-let project = import ./default.nix;
+let haskellSrc = builtins.fetchTarball "https://github.com/input-output-hk/haskell.nix/archive/cc140d188d53692a43a86cc7e4849702a3f04389.tar.gz";
+    haskellNix = import haskellSrc {};
+    # Import nixpkgs and pass the haskell.nix provided nixpkgsArgs
+    pkgs = import
+        # haskell.nix provides access to the nixpkgs pins which are used by our CI,
+        # hence you will be more likely to get cache hits when using these.
+        # But you can also just use your own, e.g. '<nixpkgs>'.
+        haskellNix.sources.nixpkgs
+        # These arguments passed to nixpkgs, include some patches and also
+        # the haskell.nix functionality itself as an overlay.
+        haskellNix.nixpkgsArgs;
+    project = pkgs.haskell-nix.stackProject {
+      src = pkgs.haskell-nix.cleanSourceHaskell {
+        name = "tacview";
+        src = ./.;
+      };
+      modules = [
+        { enableProfiling = true;
+          enableLibraryProfiling = true;
+        }
+      ];
+    };
 in project.shellFor {
     exactDeps = true;
     tools = {
       cabal = "latest";
       hlint = "latest";
       # Currently torpedoed by some build error with ghcide on 9.8
-      #haskell-language-server = "latest";
+      # haskell-language-server = "2.6.0.0";
+      # haskell-language-server = "latest";
     };
 }
