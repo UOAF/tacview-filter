0a913f472f220a1e5a44415c0ebfa9513cdf970c
Matt Kline <matt@bitbashing.io>
2025-08-14 03:15:31 -0700

    filter: Give the line we puke on

--- lib/Data/Tacview/Ignores.hs
+++ lib/Data/Tacview/Ignores.hs
@@ -5,6 +5,7 @@ module Data.Tacview.Ignores (filterLines, FilteredLines(..)) where
 
 import Control.Concurrent.Channel
 import Control.Concurrent.STM
+import Control.Exception.Safe
 import Data.HashSet (HashSet)
 import Data.HashSet qualified as HS
 import Data.Set (Set)
@@ -94,29 +95,33 @@ filterLines
     -> IO FilteredLines
 filterLines source sink = do
     endState <- stateConsumeChannel source startState $ \ !fs l -> do
-        let p = parseLine l
-            (filtered, nextState) = go p
-            -- Property lines will update our ignore lists,
-            -- then get filtered on the _updated_ version of those, fs'
-            go (PropLine pid rawProps) = (p', fs') where
-                fs' = updateIgnores pid l fs
-                p' = unlessMaybe (HS.member pid fs'.ifsIgnored) (PropLine pid $ sansG rawProps)
-            -- Skip a removal line if we're ignoring the object.
-            -- The next state is the current one with the ID removed.
-            go (RemLine r) = (p', fs') where
-                p' = unlessMaybe (HS.member r fs.ifsIgnored) p
-                fs' = removeId r fs
-            -- Skip an event if it's ignoreable.
-            go (EventLine _ is _) = (p', fs) where
-                p' = unlessMaybe (ignoreableEvent is fs) p
-            -- Pass the rest, no change to state.
-            go _ = (Just p, fs)
-        mapM_ (atomically . writeChannel' sink) filtered
+        let perLine = do
+                let p = parseLine l
+                    (filtered, nextState) = go p
+                    -- Property lines will update our ignore lists,
+                    -- then get filtered on the _updated_ version of those, fs'
+                    go (PropLine pid rawProps) = (p', fs') where
+                        fs' = updateIgnores pid l fs
+                        p' = unlessMaybe (HS.member pid fs'.ifsIgnored) (PropLine pid $ sansG rawProps)
+                    -- Skip a removal line if we're ignoring the object.
+                    -- The next state is the current one with the ID removed.
+                    go (RemLine r) = (p', fs') where
+                        p' = unlessMaybe (HS.member r fs.ifsIgnored) p
+                        fs' = removeId r fs
+                    -- Skip an event if it's ignoreable.
+                    go (EventLine _ is _) = (p', fs) where
+                        p' = unlessMaybe (ignoreableEvent is fs) p
+                    -- Pass the rest, no change to state.
+                    go _ = (Just p, fs)
+                mapM_ (atomically . writeChannel' sink) filtered
 
-        -- Add one if we filtered a line out.
-        let nextCount = fs.ifsLinesDropped + if isNothing filtered then 1 else 0
-            nextState' = nextState { ifsLinesDropped = nextCount }
-        pure nextState'
+                -- Add one if we filtered a line out.
+                let nextCount = fs.ifsLinesDropped + if isNothing filtered then 1 else 0
+                    nextState' = nextState { ifsLinesDropped = nextCount }
+                pure nextState'
+        tryAny perLine >>= \case
+            Left e -> throwString $ "on line `" <> (T.unpack l) <> "`: " <> show e
+            Right ns -> pure ns
 
     pure $ FilteredLines endState.ifsLinesDropped
 
